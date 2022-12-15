5fb032a8d36a27f57041c20cc4a843b831d4b5b7
Amélie <brun.amelie2@gmail.com>
2022-12-15 09:50:57 +0100

    Ajout des logements

--- src/pages/Accueil/Accueil.js
+++ src/pages/Accueil/Accueil.js
@@ -1,42 +1,22 @@
 import styles from "./Accueil.module.scss";
+//import React, { useEffect } from "react";
+import Logement from "../Logements/Logement";
+import { data } from "../../data/dataLogement";
 
 function Accueilpage() {
+  const logementData = data;
+
   return (
-    <div className="flex-fill">
-      <h1 className="mb-20"> Banner image</h1>
-      <div className={styles.grid}>
-        <div>ELEMENT</div>
-        <div>ELEMENT</div>
-        <div>ELEMENT</div>
-        <div>ELEMENT</div>
+    <div className="flex-fill container p-20">
+      <h1 className="my-30"> Banner image</h1>
+      <div className={`card p-20 ${styles.contentCard}`}>
+        <div className={styles.grid}>
+          {logementData.map((r) => (
+            <Logement key={r._id} title={r.title} image={r.cover} />
+          ))}
+        </div>
       </div>
     </div>
   );
 }
 export default Accueilpage;
-
-/*import React, { useEffect useState } from "react";*/
-
-/*export default function Accueilpage() {
-  const [data, setData] = useState([]);
-
- useEffect(() => {
-    fetch(
-      `https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P9+React+1/logements.json`
-    ).then((response) =>
-      response
-        .json()
-        .then(({ data }) => console.log(data))
-        .catch((error) => console.log(error))
-    );
-  }, []);
-
-  return <h2>Accueil</h2>;
-}*/
-/*export default function Accueilpage() {
-  useEffect(() => {
-    fetch("../../data/data.json").then((res) => console.log(res));
-  }, []);
-
-  return <h2>Accueil</h2>;
-}*/
